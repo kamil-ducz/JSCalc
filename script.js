c18e1c52d709bbf718df8db92825e9193da57d66
Kamil D <kamilducz1989@gmail.com>
2023-02-07 19:55:27 +0100

    Basic design and script proposition

---
+++ script.js
@@ -0,0 +1,14 @@
+function printSignToCalcHeader() 
+{
+    alert('Clicked button will be printed in the calc header');
+}
+
+function calculateResult()
+{
+    alert('Button will print calculation result in the calc header');
+}
+
+function cancelLastAction() 
+{
+    alert('Button will cancel last user operation');
+}
