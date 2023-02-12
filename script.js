57c1339f660b66f27fbbea348dc870194b22ead1
Kamil D <kamilducz1989@gmail.com>
2023-02-12 09:04:59 +0100

    Basic addition works with buffer

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var leftElement = 0;
+var rightElement = 0;
+var calculationResult = 0;
+var sign = null;
+var buffer = null;
+var currentSign = null;
+var calcState = null;
+
 function printSignToCalcHeader(sign) 
 {
     console.log('Clicked button was' + Number(sign));
@@ -7,10 +15,41 @@ function printSignToCalcHeader(sign)
 
 function calculateResult()
 {
-    alert('Button will print calculation result in the calc header');
+    var element = document.getElementById('result');
+    this.rightElement = element.value;
+    console.log('entered calculate function, rightElement='+rightElement);
+    console.log('left-ready='+this.calcState+' this.buffer='+this.buffer+' this.currentSign='+this.currentSign)
+    if (calcState = 'left-ready' && this.buffer != null & this.currentSign != null)
+    {
+        this.calculationResult = Number(leftElement) +this.sign+ Number(rightElement);
+        console.log('this.calculationResult='+this.calculationResult);
+    }
+    var element = document.getElementById('result');
+    console.log('element='+element);
+    element.value = this.calculationResult;
+    this.bufferElement = document.getElementById('buffer');
+    this.bufferElement.value += this.rightElement;
 }
 
 function cancelLastAction() 
 {
     alert('Button will cancel last user operation');
+}
+
+function handleLeftSideOfOperation(sign)
+{
+    console.log('Clicked button was ' + sign);
+    var element = document.getElementById('result');
+    console.log('element='+typeof(element));
+    this.leftElement = element.value;
+    console.log('this.leftElement='+this.leftElement);
+    this.currentSign = sign;
+    console.log('this.currentSign='+this.currentSign);
+    this.bufferElement = document.getElementById('buffer');
+    this.buffer = this.leftElement + sign;    
+    this.bufferElement.value = buffer;
+    console.log('this.buffer='+this.buffer+' buffer type='+typeof(this.buffer));
+    element.value = null;
+    calcState = 'left-ready';
+    console.log('calcState='+calcState);
 }
